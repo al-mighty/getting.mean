4cdd7e6ece6604dd26826123090b2263c035c8c8
Vyacheslav Kovalyev <Vyacheslav Kovalyev>
2017-03-19 13:54:59 +0300

    Добавлеине шаблонов представлений

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var homepageController = function (req,res) {
-    res.render('index', {title:'Express'})
-};
 
-var ctrlMain = require('../controllers/main');
+
+var ctrlLocations = require('./../controllers/locations');
+var ctrlOthers = require('./../controllers/others');
+
+
 
 
 /* GET home page. */
-router.get('/', homepageController);
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+
+//other
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
